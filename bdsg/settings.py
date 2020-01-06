fa8fcab187d10db6fc4765abf63232f35d4b3606
murie <mathews.murie@savannahinformatics.com>
2020-01-06 22:35:35 +0300

    Add donations app to installed apps

--- bdsg/settings.py
+++ bdsg/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'donations.apps.DonationsConfig',
     'pages.apps.PagesConfig',
     'listings.apps.ListingsConfig',
     'realtors.apps.RealtorsConfig',
