60f926e5c1ff917184b09e303890745d93bea5b9
murie <mathews.murie@savannahinformatics.com>
2020-01-23 15:08:51 +0300

    Demo branch

--- donations/admin.py
+++ donations/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import DonationCenter
 
 # Register your models here.
+class DonationCenterAdmin(admin.ModelAdmin):
+  list_display = ('id', 'name', 'email', 'is_approved')
+  list_display_links = ('id', 'name')
+  search_fields = ('name',)
+  list_per_page = 25
+
+admin.site.register(DonationCenter, DonationCenterAdmin)
--- donations/models.py
+++ donations/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models for donations app here.
+class DonationCenter(models.Model):
+  name = models.CharField(max_length=200)
+  photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
+  description = models.TextField(blank=True)
+  phone = models.CharField(max_length=50)
+  email = models.CharField(max_length=50)
+  is_approved = models.BooleanField(default=False)
+  is_verified = models.BooleanField(default=False)
+  def __str__(self):
+    return self.name
