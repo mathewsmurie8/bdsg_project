fe8b509632726f2bfa48b932a5c65ce1f84f9ed5
murie <mathews.murie@savannahinformatics.com>
2020-06-06 16:16:50 +0300

    Add donation center registration and SMS alerts when request is completed

--- donations/views.py
+++ donations/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import get_object_or_404, render
+from django.contrib import messages
+from django.shortcuts import get_object_or_404, render, redirect
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
-from donations.models import DonationRequest
+from donations.models import DonationRequest, DonationCenter
 
 # Create your views here.
 def index(request):
@@ -15,3 +16,31 @@ def index(request):
   }
 
   return render(request, 'listings/listings.html', context)
+
+def register_donation_center(request):
+  if request.method == 'POST':
+    organisation_name = request.POST['organisation_name']
+    email = request.POST['email']
+    phone = request.POST['phone']
+    description = request.POST['description']
+    if not email:
+      messages.error(request, 'Please provide an email address')
+      return redirect('register_donation_center')
+    if not phone:
+      messages.error(request, 'Please provide a phone number')
+      return redirect('register_donation_center')
+    if not organisation_name:
+      messages.error(request, 'Please your donation center"s name')
+      return redirect('register_donation_center')
+    if not description:
+      messages.error(request, 'Please provide a brief description of the organisation')
+      return redirect('register_donation_center')
+
+    try:
+      DonationCenter.objects.create(name=organisation_name, email=email, phone=phone, description=description)
+      messages.success(request, 'Thank you for registering your donation center. We will contact you shortly with setup instructions')
+      return redirect('index')
+    except Exception:
+      messages.error(request, 'An error occurred when you were registering your donation center')
+  else:
+    return render(request, 'accounts/register_donation_center.html')
