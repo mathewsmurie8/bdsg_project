5838b74f0d71c7a676c419a42079ec0be8f588f6
murie <mathews.murie@savannahinformatics.com>
2020-06-21 16:17:03 +0300

    Add notifications to gmail

--- bdsg/settings.py
+++ bdsg/settings.py
@@ -150,6 +150,10 @@ try:
 except ImportError:
     pass
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', '')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', '')
 GOOGLE_MAPS_API_KEY = os.getenv('GOOGLE_MAPS_API_KEY', '')
